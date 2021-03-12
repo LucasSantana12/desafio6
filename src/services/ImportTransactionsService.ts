41a5843c976d683f0b82d7f3b75bedbea3bc152a
Lucas Santana <lukkas_da_silva_santana@hotmail.com>
2021-03-12 15:47:16 -0400

    update

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,11 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
 import Transaction from '../models/Transaction';
 
+
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transaction[]> {
+    const contactsReadStream = fs.createReadStream(filePath)
   }
 }
 
